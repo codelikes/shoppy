ee0a01e7f931e8590658f38a27fb875073bbf713
Nikolay Ponich <n.ponich@gmail.com>
2023-06-02 19:15:05 +0300

    add static page

--- src/home.controller.ts
+++ src/home.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 
 @Controller()
 export class HomeController {
   @Get()
+  @Render('index')
   index() {
     return { message: 'routes: get: /, get: /page/:page, get: /:shop' };
   }
 
   @Get('page/:page')
+  @Render('page')
   page(@Param('page') page: string) {
     return { page };
   }
 
   @Get(':shop')
+  @Render('shop')
   shop(@Param('shop') shop: string) {
     return { shop };
   }
